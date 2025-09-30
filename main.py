85ece77b3ccce0dab43448e8489c8a7f0877ec62
Gabriele Fiacconi <gfiacconiuv@MacBook-Air-di-Gabriele.local>
2025-09-30 11:00:59 +0200

    updates

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from apify_client import ApifyClient
 from openai import OpenAI
 import altair as alt
 
-st.set_page_config(page_title="UV Deal Sourcing Tool)", layout="wide")
+st.set_page_config(page_title="UV Deal Sourcing Tool", layout="wide")
 
 # ----------------------------
 # Utils
